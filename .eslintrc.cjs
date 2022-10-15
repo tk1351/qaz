f8ca5e5fdeb6e6d09ca7cf424ad3241d0ad3e53d
tk1351 <takakikitsui0111@gmail.com>
2022-10-15 10:03:31 +0900

    feat: add storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,30 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: [
-		'plugin:react/recommended',
-		'xo',
-		'prettier'
-	],
-	settings: {
-		react: {
-			version: 'detect'
-		}
-	},
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: [
-		'react',
-		'@typescript-eslint',
-	],
-	rules: {
-		'react/react-in-jsx-scope': 'off'
-	},
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:react/recommended', 'xo', 'prettier', 'plugin:storybook/recommended'],
+  overrides: [
+    {
+      files: ['*.stories.@(ts|tsx|js)'],
+      extends: ['plugin:storybook/recommended']
+    }
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 'off'
+  }
 };
---
+++ src/components/atoms/Header/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+export const Header: FC = () => (
+  <header>
+    <p>QAZ</p>
+  </header>
+)
