9f02d93f0df5d8647a96c64f76ddafcbfba12d54
tk1351 <takakikitsui0111@gmail.com>
2022-10-16 12:35:12 +0900

    feat: App.tsx のリスト表示表記修正

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,51 @@ import { FC } from 'react'
 import styled from 'styled-components'
 import tokens from '../data/build/tokens'
 import { ReactComponent as Search } from './assets/icons/search_FILL0_wght400_GRAD0_opsz48.svg'
+import { convertToUpperCase } from './features/convertToUpperCase'
+import { mappedNavigation } from './constants/navigation'
 
-export const App: FC = () => (
-  <StyledWrapper>
-    <StyledHeader>
-      <StyledLink href="#">QAZ</StyledLink>
-      <StyledForm>
-        <StyledInputWrapper>
-          <StyledInput type="text" placeholder="Search" />
-          <StyledSvg viewBox="-25 -25 100 100" />
-        </StyledInputWrapper>
-      </StyledForm>
-      <StyledNav>
-        <StyledList>
-          <li>
-            <StyledNavLink href="">NOW SHOWING</StyledNavLink>
-          </li>
-          <li>
-            <StyledNavLink href="">WATCH LIST</StyledNavLink>
-          </li>
-        </StyledList>
-      </StyledNav>
-    </StyledHeader>
-  </StyledWrapper>
-)
+type NavigationListItem = ReadonlyArray<{ name: string; href: string }>
+
+export const App: FC = () => {
+  const navigationListItem: NavigationListItem = [
+    {
+      name: mappedNavigation.nowShowing.name,
+      href: mappedNavigation.nowShowing.href
+    },
+    {
+      name: mappedNavigation.watchList.name,
+      href: mappedNavigation.watchList.href
+    }
+  ]
+
+  const mappedNavigationListItem = (itemList: NavigationListItem) =>
+    itemList.map(({ name, href }) => ({
+      name: convertToUpperCase(name),
+      href
+    }))
+  return (
+    <StyledWrapper>
+      <StyledHeader>
+        <StyledLink href="#">QAZ</StyledLink>
+        <StyledForm>
+          <StyledInputWrapper>
+            <StyledInput type="text" placeholder="Search" />
+            <StyledSvg viewBox="-25 -25 100 100" />
+          </StyledInputWrapper>
+        </StyledForm>
+        <StyledNav>
+          <StyledList>
+            {mappedNavigationListItem(navigationListItem).map((item) => (
+              <li key={item.name}>
+                <StyledNavLink href={item.href}>{item.name}</StyledNavLink>
+              </li>
+            ))}
+          </StyledList>
+        </StyledNav>
+      </StyledHeader>
+    </StyledWrapper>
+  )
+}
 
 const StyledWrapper = styled.div`
   min-width: 100vw;
