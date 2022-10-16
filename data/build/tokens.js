5d87b653ea9013dcac8e7ac3dc19bda234164e67
tk1351 <takakikitsui0111@gmail.com>
2022-10-16 10:29:00 +0900

    feat: update sd build files

---
+++ data/build/tokens.js
@@ -0,0 +1,397 @@
+/**
+ * Do not edit directly
+ * Generated on Sat, 15 Oct 2022 23:39:17 GMT
+ */
+
+export default {
+  global: {
+    Black: {
+      1: {
+        value: '#222222',
+        type: 'color',
+        description: 'Main Color',
+        filePath: './data/tokens.json',
+        isSource: true,
+        original: {
+          value: '#222222',
+          type: 'color',
+          description: 'Main Color'
+        },
+        name: 'GlobalBlack1',
+        attributes: {
+          category: 'global',
+          type: 'Black',
+          item: '1'
+        },
+        path: ['global', 'Black', '1']
+      },
+      2: {
+        value: '#3A3A3A',
+        type: 'color',
+        description: 'Main Color',
+        filePath: './data/tokens.json',
+        isSource: true,
+        original: {
+          value: '#3A3A3A',
+          type: 'color',
+          description: 'Main Color'
+        },
+        name: 'GlobalBlack2',
+        attributes: {
+          category: 'global',
+          type: 'Black',
+          item: '2'
+        },
+        path: ['global', 'Black', '2']
+      },
+      3: {
+        value: '#545454',
+        type: 'color',
+        description: 'Main Color',
+        filePath: './data/tokens.json',
+        isSource: true,
+        original: {
+          value: '#545454',
+          type: 'color',
+          description: 'Main Color'
+        },
+        name: 'GlobalBlack3',
+        attributes: {
+          category: 'global',
+          type: 'Black',
+          item: '3'
+        },
+        path: ['global', 'Black', '3']
+      },
+      4: {
+        value: '#F2F2F2',
+        type: 'color',
+        description: 'Main Color',
+        filePath: './data/tokens.json',
+        isSource: true,
+        original: {
+          value: '#F2F2F2',
+          type: 'color',
+          description: 'Main Color'
+        },
+        name: 'GlobalBlack4',
+        attributes: {
+          category: 'global',
+          type: 'Black',
+          item: '4'
+        },
+        path: ['global', 'Black', '4']
+      }
+    },
+    'Dark Green': {
+      1: {
+        value: '#044343',
+        type: 'color',
+        description: 'Sub Color',
+        filePath: './data/tokens.json',
+        isSource: true,
+        original: {
+          value: '#044343',
+          type: 'color',
+          description: 'Sub Color'
+        },
+        name: 'GlobalDarkGreen1',
+        attributes: {
+          category: 'global',
+          type: 'Dark Green',
+          item: '1'
+        },
+        path: ['global', 'Dark Green', '1']
+      },
+      2: {
+        value: '#076E6E',
+        type: 'color',
+        description: 'Sub Color',
+        filePath: './data/tokens.json',
+        isSource: true,
+        original: {
+          value: '#076E6E',
+          type: 'color',
+          description: 'Sub Color'
+        },
+        name: 'GlobalDarkGreen2',
+        attributes: {
+          category: 'global',
+          type: 'Dark Green',
+          item: '2'
+        },
+        path: ['global', 'Dark Green', '2']
+      },
+      3: {
+        value: '#0A9E9E',
+        type: 'color',
+        description: 'Sub Color',
+        filePath: './data/tokens.json',
+        isSource: true,
+        original: {
+          value: '#0A9E9E',
+          type: 'color',
+          description: 'Sub Color'
+        },
+        name: 'GlobalDarkGreen3',
+        attributes: {
+          category: 'global',
+          type: 'Dark Green',
+          item: '3'
+        },
+        path: ['global', 'Dark Green', '3']
+      },
+      4: {
+        value: '#E7FDFD',
+        type: 'color',
+        description: 'Sub Color',
+        filePath: './data/tokens.json',
+        isSource: true,
+        original: {
+          value: '#E7FDFD',
+          type: 'color',
+          description: 'Sub Color'
+        },
+        name: 'GlobalDarkGreen4',
+        attributes: {
+          category: 'global',
+          type: 'Dark Green',
+          item: '4'
+        },
+        path: ['global', 'Dark Green', '4']
+      }
+    },
+    Green: {
+      1: {
+        value: '#045757',
+        type: 'color',
+        description: 'Sub Color',
+        filePath: './data/tokens.json',
+        isSource: true,
+        original: {
+          value: '#045757',
+          type: 'color',
+          description: 'Sub Color'
+        },
+        name: 'GlobalGreen1',
+        attributes: {
+          category: 'global',
+          type: 'Green',
+          item: '1'
+        },
+        path: ['global', 'Green', '1']
+      },
+      2: {
+        value: '#068383',
+        type: 'color',
+        description: 'Sub Color',
+        filePath: './data/tokens.json',
+        isSource: true,
+        original: {
+          value: '#068383',
+          type: 'color',
+          description: 'Sub Color'
+        },
+        name: 'GlobalGreen2',
+        attributes: {
+          category: 'global',
+          type: 'Green',
+          item: '2'
+        },
+        path: ['global', 'Green', '2']
+      },
+      3: {
+        value: '#08B4B4',
+        type: 'color',
+        description: 'Sub Color',
+        filePath: './data/tokens.json',
+        isSource: true,
+        original: {
+          value: '#08B4B4',
+          type: 'color',
+          description: 'Sub Color'
+        },
+        name: 'GlobalGreen3',
+        attributes: {
+          category: 'global',
+          type: 'Green',
+          item: '3'
+        },
+        path: ['global', 'Green', '3']
+      },
+      4: {
+        value: '#E6FDFD',
+        type: 'color',
+        description: 'Sub Color',
+        filePath: './data/tokens.json',
+        isSource: true,
+        original: {
+          value: '#E6FDFD',
+          type: 'color',
+          description: 'Sub Color'
+        },
+        name: 'GlobalGreen4',
+        attributes: {
+          category: 'global',
+          type: 'Green',
+          item: '4'
+        },
+        path: ['global', 'Green', '4']
+      }
+    },
+    White: {
+      1: {
+        value: '#E4E4E4',
+        type: 'color',
+        description: 'Text Color',
+        filePath: './data/tokens.json',
+        isSource: true,
+        original: {
+          value: '#E4E4E4',
+          type: 'color',
+          description: 'Text Color'
+        },
+        name: 'GlobalWhite1',
+        attributes: {
+          category: 'global',
+          type: 'White',
+          item: '1'
+        },
+        path: ['global', 'White', '1']
+      },
+      2: {
+        value: '#FCFCFC',
+        type: 'color',
+        description: 'Text Color',
+        filePath: './data/tokens.json',
+        isSource: true,
+        original: {
+          value: '#FCFCFC',
+          type: 'color',
+          description: 'Text Color'
+        },
+        name: 'GlobalWhite2',
+        attributes: {
+          category: 'global',
+          type: 'White',
+          item: '2'
+        },
+        path: ['global', 'White', '2']
+      },
+      3: {
+        value: '#F2F2F2',
+        type: 'color',
+        description: 'Text Color',
+        filePath: './data/tokens.json',
+        isSource: true,
+        original: {
+          value: '#F2F2F2',
+          type: 'color',
+          description: 'Text Color'
+        },
+        name: 'GlobalWhite3',
+        attributes: {
+          category: 'global',
+          type: 'White',
+          item: '3'
+        },
+        path: ['global', 'White', '3']
+      },
+      4: {
+        value: '#FFFFFF',
+        type: 'color',
+        description: 'Text Color',
+        filePath: './data/tokens.json',
+        isSource: true,
+        original: {
+          value: '#FFFFFF',
+          type: 'color',
+          description: 'Text Color'
+        },
+        name: 'GlobalWhite4',
+        attributes: {
+          category: 'global',
+          type: 'White',
+          item: '4'
+        },
+        path: ['global', 'White', '4']
+      }
+    },
+    sm: {
+      value: '16',
+      type: 'fontSizes',
+      description: 'small size',
+      filePath: './data/tokens.json',
+      isSource: true,
+      original: {
+        value: '16',
+        type: 'fontSizes',
+        description: 'small size'
+      },
+      name: 'GlobalSm',
+      attributes: {
+        category: 'global',
+        type: 'sm'
+      },
+      path: ['global', 'sm']
+    },
+    lg: {
+      value: '20',
+      type: 'fontSizes',
+      description: 'large size',
+      filePath: './data/tokens.json',
+      isSource: true,
+      original: {
+        value: '20',
+        type: 'fontSizes',
+        description: 'large size'
+      },
+      name: 'GlobalLg',
+      attributes: {
+        category: 'global',
+        type: 'lg'
+      },
+      path: ['global', 'lg']
+    },
+    xl: {
+      value: '32',
+      type: 'fontSizes',
+      description: 'xl size',
+      filePath: './data/tokens.json',
+      isSource: true,
+      original: {
+        value: '32',
+        type: 'fontSizes',
+        description: 'xl size'
+      },
+      name: 'GlobalXl',
+      attributes: {
+        category: 'global',
+        type: 'xl'
+      },
+      path: ['global', 'xl']
+    },
+    '2xl': {
+      value: '48',
+      type: 'fontSizes',
+      description: '2xl size',
+      filePath: './data/tokens.json',
+      isSource: true,
+      original: {
+        value: '48',
+        type: 'fontSizes',
+        description: '2xl size'
+      },
+      name: 'Global2xl',
+      attributes: {
+        category: 'global',
+        type: '2xl'
+      },
+      path: ['global', '2xl']
+    }
+  },
+  $themes: [],
+  $metadata: {
+    tokenSetOrder: ['global']
+  }
+}
