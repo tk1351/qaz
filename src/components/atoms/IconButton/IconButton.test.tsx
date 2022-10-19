c3577fadf22aa9a1fbf4b32ad582a08a63ed6fa7
bitaKitsui <takaki_kitsui@bita.jp>
2022-10-19 15:10:37 +0900

    feat: disabled 属性追加

--- src/components/atoms/IconButton/IconButton.test.tsx
+++ src/components/atoms/IconButton/IconButton.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { IconButton } from './IconButton'
 
-const setup = () => {
+const setup = (disabled: boolean) => {
   const mockFunction = vi.fn()
   const utils = render(
     <IconButton
@@ -11,6 +11,7 @@ const setup = () => {
       buttonHeight={32}
       svgWidth={24}
       svgHeight={24}
+      disabled={disabled}
       onClick={mockFunction}
     >
       Icon Button
@@ -22,16 +23,32 @@ const setup = () => {
 describe('Testing IconButton Component', () => {
   describe('role', () => {
     it('role="button" が含まれていること', () => {
-      setup()
+      setup(false)
       const button = screen.getByRole('button')
       expect(button).toBeInTheDocument()
+    })
+  })
+
+  describe('Props', () => {
+    describe('disabled', () => {
+      it('disabled が true の場合、 disabled 属性が付与されていること', () => {
+        setup(true)
+        const button = screen.getByRole('button')
+        expect(button).toBeDisabled()
+      })
+
+      it('disabled が false の場合、 disabled 属性が付与されていないこと', () => {
+        setup(false)
+        const button = screen.getByRole('button')
+        expect(button).toBeEnabled()
+      })
     })
   })
 
   describe('Event', () => {
     describe('onClick', () => {
       it('IconButton コンポーネントをクリックしたときに、 onClick が1度発動すること', async () => {
-        const { mockFunction } = setup()
+        const { mockFunction } = setup(false)
         await userEvent.click(screen.getByRole('button'))
         expect(mockFunction).toHaveBeenCalledTimes(1)
       })
