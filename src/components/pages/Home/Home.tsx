296ea357159ee2762a582ab766b3dc6f225f0387
bitaKitsui <takaki_kitsui@bita.jp>
2022-10-30 18:29:15 +0900

    feat: add home page

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -10,10 +10,11 @@ import {
   useMovieDetail,
   useNowPlaying
 } from '../../../features/api/movie'
+import { MovieList } from '../../commons/MovieList/MovieList'
+import { MovieCardType } from '../../../shared/types'
+import { Movie, MovieCredits } from '../../../../api/types/movies'
 
 export const Home: FC = () => {
-  const { data: nowPlayingData } = useNowPlaying()
-
   const getRandomArbitrary = (min: number, max: number) => {
     min = Math.ceil(min)
     max = Math.ceil(max)
@@ -21,24 +22,75 @@ export const Home: FC = () => {
   }
 
   const index = getRandomArbitrary(0, 19)
-  const nowMovieImage = nowPlayingData
-    ? `https://image.tmdb.org/t/p/original${nowPlayingData[index].backdrop_path}`
-    : ''
+  const imageUrl = (path: string | null) =>
+    `https://image.tmdb.org/t/p/original${path}`
+
+  const { data: nowPlayingData } = useNowPlaying()
 
   if (nowPlayingData) {
     // eslint-disable-next-line camelcase
-    const { id, title, overview, release_date } = nowPlayingData[index]
+    const { id, title, overview, release_date, backdrop_path } =
+      nowPlayingData[index]
+    const image = imageUrl(backdrop_path)
     const year = nowPlayingData ? excludeFromString(release_date, '-') : ''
 
-    const { data: movieDetail } = useMovieDetail(id)
-    const { data: movieCredits } = useMovieCredits(id)
+    const nowPlayingMovieIds = nowPlayingData.map((data) => data.id)
 
-    const crew = movieCredits?.crew
-    const directorName = crew?.find((person) => person.job === 'Director')?.name
-    const country = movieDetail?.production_countries[0]?.name
+    const nowPlayingDetailsAndCredits = nowPlayingMovieIds.map((id) => {
+      const { data: movieDetails } = useMovieDetail(id)
+      const { data: movieCredits } = useMovieCredits(id)
+      return {
+        movieDetails,
+        movieCredits
+      }
+    })
+
+    const findMovieById = (
+      id: number,
+      movies: {
+        movieDetails: Movie | undefined
+        movieCredits: MovieCredits | undefined
+      }[]
+    ) =>
+      movies.find(({ movieDetails }) => movieDetails && movieDetails.id === id)
+
+    const heroDirectorName = findMovieById(
+      id,
+      nowPlayingDetailsAndCredits
+    )?.movieCredits?.crew?.find((person) => person.job === 'Director')?.name
+
+    const heroCountry = findMovieById(id, nowPlayingDetailsAndCredits)
+      ?.movieDetails?.production_countries[0]?.name
+
+    const movieCardDirectorName = (id: number) => {
+      const target = findMovieById(id, nowPlayingDetailsAndCredits)
+
+      return target?.movieCredits?.crew.find(
+        (person) => person.job === 'Director'
+      )?.name
+    }
+
+    const movieCardCountry = (id: number) => {
+      const target = findMovieById(id, nowPlayingDetailsAndCredits)
+
+      return target?.movieDetails?.production_countries[0]?.name
+    }
+
+    const movies: MovieCardType[] = nowPlayingData.map(
+      // eslint-disable-next-line camelcase
+      ({ id, title, release_date, backdrop_path }) => ({
+        id,
+        title,
+        directorName: movieCardDirectorName(id) || '',
+        country: movieCardCountry(id) || '',
+        year: excludeFromString(release_date, '-'),
+        imageURL: imageUrl(backdrop_path)
+      })
+    )
+
     return (
       <StyledHomeWrapper>
-        <HeroImage imageURL={nowMovieImage}>
+        <HeroImage imageURL={image}>
           <StyledDescriptionWrapper>
             <Stack space="6px">
               <StyledNowPlayingText>Now Playing</StyledNowPlayingText>
@@ -51,16 +103,24 @@ export const Home: FC = () => {
                     fontWeight: 'bold'
                   }}
                 >
-                  {directorName}
+                  {heroDirectorName}
                 </span>
                 <StyledCountryAndYearText>
-                  {country} {year}
+                  {heroCountry} {year}
                 </StyledCountryAndYearText>
               </Cluster>
               <StyledOverview>{overview}</StyledOverview>
             </Stack>
           </StyledDescriptionWrapper>
         </HeroImage>
+        <StyledNowPlayingListWrapper>
+          <MovieList
+            rowGap="10px"
+            columnGap="10px"
+            minLength="300px"
+            movies={movies}
+          />
+        </StyledNowPlayingListWrapper>
       </StyledHomeWrapper>
     )
   }
@@ -74,7 +134,7 @@ const StyledHomeWrapper = styled.div`
 
 const StyledDescriptionWrapper = styled.div`
   height: 100%;
-  width: 400px;
+  width: 450px;
 `
 
 const StyledNowPlayingText = styled.p`
@@ -98,4 +158,10 @@ const StyledOverview = styled.p`
 
 const StyledCountryAndYearText = styled.span`
   color: ${tokens.global.White['1'].value};
+`
+
+const StyledNowPlayingListWrapper = styled.div`
+  min-height: 500px;
+  padding: 40px 160px;
+  background-color: ${tokens.global.Black['1'].value};
 `
